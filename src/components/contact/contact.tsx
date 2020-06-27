febd4f22a708324c04b126a4da5197360463a9f8
Tim D'hoore <timdhoore@gmail.com>
2020-06-27 17:17:06 +0200

    added the base of the contact html

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -2,7 +2,34 @@ import React from "react";
 import { props } from "./types";
 
 const Contact = (props: props) => {
-  return <div>Contact</div>;
+  return (
+    <article className="contactArticle">
+      <header>
+        <h2 className="hide">Contact</h2>
+        <span>Inbox 0</span>
+        <p>Love to hear from you</p>
+      </header>
+      <a href="mailto:" className="defaultButton">
+        Leave a message
+      </a>
+      <ul className="socialLinks">
+        <li>
+          <a href="" target="_blank">
+            <span>Link</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Link</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Link</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Link</span>
+          </a>
+        </li>
+      </ul>
+    </article>
+  );
 };
 
 export default Contact;
