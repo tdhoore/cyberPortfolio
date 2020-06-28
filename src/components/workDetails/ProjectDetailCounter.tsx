96dd10f47c0a90e7ba33333fc2ae6ba7e70ee298
Tim D'hoore <timdhoore@gmail.com>
2020-06-28 22:42:06 +0200

    added more to detailProject

--- src/components/workDetails/ProjectDetailCounter.tsx
+++ src/components/workDetails/ProjectDetailCounter.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ProjectDetailCounter = (props: any) => {
   //gebruik push url
   return (
-    <div className="projectCounter">
+    <div className="projectDetailCounter">
       <Link to="" className="diamondButton"></Link>
       <div className="pixFont counter blurEffectText">*___</div>
       <Link to="" className="diamondButton"></Link>
--- src/components/workDetails/WorkDetails.tsx
+++ src/components/workDetails/WorkDetails.tsx
@@ -8,39 +8,41 @@ const WorkDetails = (props: props) => {
       <header>
         <h2>title</h2>
       </header>
-      <section className="projectInfoSection">
-        <header>
-          <h3>Project info</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit maiores
-            aliquam quam voluptate sapiente mollitia corrupti quas aut tempora
-            nesciunt? Qui ullam voluptatem molestias alias officiis sit pariatur
-            culpa animi.
-          </p>
-        </header>
-        <img src="" alt="" />
-        <section>
-          <header className="hide">
-            <h4>main information</h4>
+      <div className="workDetailContent">
+        <section className="projectInfoSection">
+          <header>
+            <h3>Project info</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              maiores aliquam quam voluptate sapiente mollitia corrupti quas aut
+              tempora nesciunt? Qui ullam voluptatem molestias alias officiis
+              sit pariatur culpa animi.
+            </p>
           </header>
-          <dl>
-            <div>
-              <dt>Roll</dt>
-              <dd>roll</dd>
-            </div>
-            <div>
-              <dt>Client</dt>
-              <dd>Awsome corp</dd>
-            </div>
-          </dl>
+          <img src="" alt="" />
+          <section>
+            <header className="hide">
+              <h4>main information</h4>
+            </header>
+            <dl className="rollInformation">
+              <div>
+                <dt>Roll</dt>
+                <dd>roll</dd>
+              </div>
+              <div>
+                <dt>Client</dt>
+                <dd>Awsome corp</dd>
+              </div>
+            </dl>
+          </section>
         </section>
-      </section>
-      <section className="screenshotSection">
-        <header>
-          <h3>screenshot</h3>
-        </header>
-        <img src="" alt="" />
-      </section>
+        <section className="screenshotSection">
+          <header>
+            <h3>screenshot</h3>
+          </header>
+          <img src="" alt="" />
+        </section>
+      </div>
       <ProjectDetailCounter />
     </article>
   );
