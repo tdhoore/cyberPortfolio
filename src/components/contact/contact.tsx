2c38f0e73d32db0ad4ea114d47370624e598d0d8
Tim D'hoore <timdhoore@gmail.com>
2020-07-02 20:42:59 +0200

    created base for the contact page css

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -6,34 +6,44 @@ const Contact = (props: props) => {
     <article className="contactArticle">
       <header>
         <h2 className="hide">Contact</h2>
+      </header>
+      <div className="letter">
         <span>Inbox 0</span>
         <p>Love to hear from you</p>
-      </header>
-      <a href="mailto:" className="defaultButton">
-        Leave a message
-      </a>
-      <ul className="socialLinks">
-        <li>
-          <a href="" target="_blank">
-            <span>Link</span>
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank">
-            <span>Link</span>
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank">
-            <span>Link</span>
-          </a>
-        </li>
-        <li>
-          <a href="" target="_blank">
-            <span>Link</span>
-          </a>
-        </li>
-      </ul>
+      </div>
+      <div className="contactInfo">
+        <a href="mailto:" className="defaultButton">
+          Leave a message
+        </a>
+        <ul className="socialLinks">
+          <li>
+            <a href="" target="_blank">
+              <span>Link</span>
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank">
+              <span>Link</span>
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank">
+              <span>Link</span>
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank">
+              <span>Link</span>
+            </a>
+          </li>
+        </ul>
+        <div className="pixFont blurEffectText decoNumbers">
+          <div>541</div>
+          <div>541</div>
+          <div>541</div>
+          <div>541</div>
+        </div>
+      </div>
     </article>
   );
 };
