7c444dd584f63ecbc77f9a4641dcf84aba4e402c
Tim D'hoore <timdhoore@gmail.com>
2020-07-02 16:06:39 +0200

    fixed centering on about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,15 +4,17 @@ import { props } from "./types";
 const About = (props: props) => {
   return (
     <article className="aboutArticle">
-      <header>
-        <h2>I'm Tim D'hoore</h2>
-      </header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-        labore inventore ex quas aliquam nesciunt saepe in numquam cupiditate
-        vel, aliquid suscipit error itaque culpa quo. Vitae est perferendis
-        tempore.
-      </p>
+      <div className="titleAndInfo">
+        <header>
+          <h2>I'm Tim D'hoore</h2>
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+          labore inventore ex quas aliquam nesciunt saepe in numquam cupiditate
+          vel, aliquid suscipit error itaque culpa quo. Vitae est perferendis
+          tempore.
+        </p>
+      </div>
       <div className="imageHolder">
         <img src="https://via.placeholder.com/236x353" alt="" />
       </div>
