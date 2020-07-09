278d21fe573c17cecb49593168e9393bb8ee7216
Tim D'hoore <timdhoore@gmail.com>
2020-07-09 17:28:06 +0200

    added scroll button on home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -6,17 +6,22 @@ const Home = (props: props) => {
   return (
     <article className="homeArticle">
       <div className="wrapper">
-        <div className="intro">
-          <header>
-            <h2>Tim D'hoore</h2>
-            <p>Creative frontend developer</p>
-          </header>
-          <p className="shortInfo">
-            Delectus reiciendis impedit possimus dolor esse vitae itaque ratione
-            natus labore voluptatibus!
-          </p>
+        <div className="content">
+          <div className="intro">
+            <header>
+              <h2>Tim D'hoore</h2>
+              <p>Creative frontend developer</p>
+            </header>
+            <p className="shortInfo">
+              Delectus reiciendis impedit possimus dolor esse vitae itaque
+              ratione natus labore voluptatibus!
+            </p>
+          </div>
+          <img src={logo} alt="Tim D'hoore logo" />
         </div>
-        <img src={logo} alt="Tim D'hoore logo" />
+        <div className="scrollNextPage">
+          <a className="">Scroll for more</a>
+        </div>
       </div>
     </article>
   );
