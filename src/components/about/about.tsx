7f30fdd2351093b8fe3f4e61bf5036e19e1d3fb4
Tim D'hoore <timdhoore@gmail.com>
2020-08-27 13:51:08 +0200

    all pages have base animations

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { props } from "./types";
+import { motion } from "framer-motion";
+import { panelAnim } from "../anim/animationPresets";
 
 const About = (props: props) => {
   return (
     <article className="aboutArticle">
-      <div className="wrapper accentBarAfter">
-        <div className="titleAndInfo panel">
+      <div className="wrapper">
+        <motion.div className="titleAndInfo panel" {...panelAnim}>
           <header>
             <h2>
               Hi, I'm
@@ -14,18 +16,31 @@ const About = (props: props) => {
             </h2>
             <p>sub title</p>
           </header>
-          <p>
+          <p className="aboutInfo">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Consectetur labore inventore ex quas aliquam nesciunt saepe in
             numquam cupiditate vel, aliquid suscipit error itaque culpa quo.
             Vitae est perferendis tempore.
           </p>
-        </div>
-        <div className="imageHolder panel">
+        </motion.div>
+        <motion.div
+          className="imageHolder panel"
+          {...panelAnim}
+          transition={{ ...panelAnim.transition, delay: 0.4 }}
+        >
           <img src="https://via.placeholder.com/236x353" alt="" />
-          <div className="decoPanel aboutDeco2"></div>
-        </div>
-        <div className="panel aboutDeco1"></div>
+          <motion.div
+            className="decoPanel aboutDeco2"
+            {...panelAnim}
+            transition={{ ...panelAnim.transition, delay: 0.5 }}
+          ></motion.div>
+        </motion.div>
+        <motion.div
+          className="panel aboutDeco1"
+          {...panelAnim}
+          transition={{ ...panelAnim.transition, delay: 0.6 }}
+        ></motion.div>
+        <div className="accentBarAfter"></div>
       </div>
     </article>
   );
