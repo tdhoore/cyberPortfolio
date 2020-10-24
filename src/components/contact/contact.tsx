546bcf94c242c1c4d97e9cd23b01bd3a828c1576
Tim D'hoore <timdhoore@gmail.com>
2020-10-24 18:00:11 +0200

    added some social links

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -10,6 +10,7 @@ const Contact = () => {
           <h2>I'm still looking for work</h2>
           <p>Have anything?</p>
         </header>
+        <p>mail@mail.com</p>
         <a href="mailto:" className="defaultButton">
           Leave a message
         </a>
@@ -24,23 +25,23 @@ const Contact = () => {
         </header>
         <ul className="socialLinks">
           <li>
-            <a href="#" target="_blank" className="linkButton">
-              <span>Link</span>
+            <a href="mailto:tim@mail.com?subject=I%20need%20something%20awsome%20made!!!" target="_blank" className="linkButton">
+              <span>mymail@mail.com</span>
             </a>
           </li>
           <li>
-            <a href="#" target="_blank" className="linkButton">
-              <span>Link</span>
+            <a href="https://www.behance.net/tdhgraphics" target="_blank" className="linkButton">
+              <span>Behance</span>
             </a>
           </li>
           <li>
-            <a href="#" target="_blank" className="linkButton">
-              <span>Link</span>
+            <a href="https://www.linkedin.com/in/timdhoore/" target="_blank" className="linkButton">
+              <span>linkedin</span>
             </a>
           </li>
           <li>
-            <a href="#" target="_blank" className="linkButton">
-              <span>Link</span>
+            <a href="https://www.artstation.com/tdhgraphics" target="_blank" className="linkButton">
+              <span>Artstation</span>
             </a>
           </li>
         </ul>
